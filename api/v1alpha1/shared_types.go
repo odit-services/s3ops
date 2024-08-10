a3f967778167689ecbf1fa20eb74d44542dbbd0a
Nicolai Ort <info@nicolai-ort.com>
2024-08-10 17:46:37 +0200

    feat(s3server_controller): Implement s3server status check on ressource creation

---
+++ api/v1alpha1/shared_types.go
@@ -0,0 +1,14 @@
+package v1alpha1
+
+// Implement the condition enums
+const (
+	ConditionReady       = "Ready"
+	ConditionFailed      = "Failed"
+	ConditionReconciling = "Reconciling"
+)
+
+// Implement the reason enums
+const (
+	ReasonNotFound = "NotFound"
+	ReasonOffline  = "Offline"
+)
--- internal/controller/s3server_controller.go
+++ internal/controller/s3server_controller.go
@@ -18,24 +18,36 @@ package controller
 
 import (
 	"context"
+	"fmt"
+	"os"
+	"strings"
 
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
 
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	s3oditservicesv1alpha1 "github.com/odit-services/s3ops/api/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 // S3ServerReconciler reconciles a S3Server object
 type S3ServerReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	logger *zap.SugaredLogger
 }
 
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3servers,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3servers/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3servers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3servers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3servers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3servers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=core,resources=events,verbs=create;patch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -48,15 +60,97 @@ type S3ServerReconciler struct {
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *S3ServerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
+	r.logger.Infow("Reconciling S3Server", "name", req.Name, "namespace", req.Namespace)
 
-	// TODO(user): your logic here
+	s3Server := &s3oditservicesv1alpha1.S3Server{}
+	err := r.Get(ctx, req.NamespacedName, s3Server)
+	if err != nil {
+		r.logger.Errorw("Failed to get S3Server resource", "name", req.Name, "namespace", req.Namespace, "error", err)
+		s3Server.Status.Conditions = append(s3Server.Status.Conditions, metav1.Condition{
+			Type:    s3oditservicesv1alpha1.ConditionFailed,
+			Status:  metav1.ConditionFalse,
+			Reason:  s3oditservicesv1alpha1.ReasonNotFound,
+			Message: "S3Server resource not found",
+		})
+		r.Status().Update(ctx, s3Server)
+		return ctrl.Result{}, err
+	}
+
+	s3Server.Status.Conditions = append(s3Server.Status.Conditions, metav1.Condition{
+		Type:    s3oditservicesv1alpha1.ConditionReconciling,
+		Status:  metav1.ConditionUnknown,
+		Message: fmt.Sprintf("Reconciling S3Server %s", s3Server.Name),
+	})
+	err = r.Status().Update(ctx, s3Server)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	minioClient, err := minio.New(s3Server.Spec.Endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(s3Server.Spec.Auth.AccessKey, s3Server.Spec.Auth.SecretKey, ""),
+		Secure: s3Server.Spec.TLS,
+	})
+	if err != nil {
+		r.logger.Errorw("Failed to create Minio client for S3Server", "name", req.Name, "namespace", req.Namespace, "error", err)
+		s3Server.Status.Conditions = append(s3Server.Status.Conditions, metav1.Condition{
+			Type:    s3oditservicesv1alpha1.ConditionFailed,
+			Status:  metav1.ConditionFalse,
+			Message: fmt.Sprintf("Failed to create Minio client: %v", err),
+		})
+		r.Status().Update(ctx, s3Server)
+		return ctrl.Result{}, err
+	}
+
+	minioOnline := minioClient.IsOnline()
+	if !minioOnline {
+		r.logger.Errorw("Minio server is offline for S3Server", "name", req.Name, "namespace", req.Namespace)
+		s3Server.Status.Conditions = append(s3Server.Status.Conditions, metav1.Condition{
+			Type:    s3oditservicesv1alpha1.ConditionFailed,
+			Status:  metav1.ConditionFalse,
+			Reason:  s3oditservicesv1alpha1.ReasonOffline,
+			Message: "Minio server is offline",
+		})
+		r.Status().Update(ctx, s3Server)
+		return ctrl.Result{}, fmt.Errorf("minio server is offline")
+	}
+
+	r.logger.Infow("Finished reconciling S3Server", "name", req.Name, "namespace", req.Namespace)
+	s3Server.Status.Conditions = append(s3Server.Status.Conditions, metav1.Condition{
+		Type:    s3oditservicesv1alpha1.ConditionReady,
+		Status:  metav1.ConditionTrue,
+		Message: "S3Server is ready",
+	})
+	err = r.Status().Update(ctx, s3Server)
+	if err != nil {
+		r.logger.Errorw("Failed to update S3Server status", "name", req.Name, "namespace", req.Namespace, "error", err)
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *S3ServerReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	lovLevel := os.Getenv("LOG_LEVEL")
+	if lovLevel == "" {
+		lovLevel = "INFO"
+	}
+
+	var zapLogLevel zapcore.Level
+	err := zapLogLevel.UnmarshalText([]byte(strings.ToLower(lovLevel)))
+	if err != nil {
+		zapLogLevel = zapcore.InfoLevel
+	}
+
+	zapConfig := zap.NewProductionConfig()
+	zapConfig.Level = zap.NewAtomicLevelAt(zapLogLevel)
+	zapLogger, _ := zapConfig.Build()
+	defer zapLogger.Sync()
+	r.logger = zapLogger.Sugar()
+
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&s3oditservicesv1alpha1.S3Server{}).
+		WithEventFilter(predicate.Or(predicate.GenerationChangedPredicate{}, predicate.LabelChangedPredicate{})).
 		Complete(r)
 }
