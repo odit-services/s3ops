789e2e9c8c2cb7e0bf3762f9f6d0f21eb3f7354c
Nicolai Ort <info@nicolai-ort.com>
2024-08-10 19:36:11 +0200

    style(s3bucket_controller): Removed unused comments

--- internal/controller/s3bucket_controller.go
+++ internal/controller/s3bucket_controller.go
@@ -33,19 +33,10 @@ type S3BucketReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets/finalizers,verbs=update
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=s3.odit.services,resources=s3buckets/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the S3Bucket object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.17.3/pkg/reconcile
 func (r *S3BucketReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
