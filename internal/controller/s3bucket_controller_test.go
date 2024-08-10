f950890846f5158782744c438f825f674a820b46
Nicolai Ort <info@nicolai-ort.com>
2024-08-10 21:02:36 +0200

    test(s3bucket_controller): Added tests for happy path

--- internal/controller/s3bucket_controller_test.go
+++ internal/controller/s3bucket_controller_test.go
@@ -21,64 +21,104 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-
+	"go.uber.org/zap"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	ctrl "sigs.k8s.io/controller-runtime"
 
 	s3oditservicesv1alpha1 "github.com/odit-services/s3ops/api/v1alpha1"
+	"github.com/odit-services/s3ops/internal/controller/mocks"
 )
 
-var _ = Describe("S3Bucket Controller", func() {
-	Context("When reconciling a resource", func() {
-		const resourceName = "test-resource"
+var _ = Describe("S3Bucket Controller", Ordered, func() {
+	ctx := context.Background()
+	s3MockEnv := mocks.DefaultMockEnvs()
+	var s3MockSpy *mocks.S3ClientMockSpy
+	var s3Server *s3oditservicesv1alpha1.S3Server
+	var testReconciler *S3BucketReconciler
 
-		ctx := context.Background()
-
-		typeNamespacedName := types.NamespacedName{
-			Name:      resourceName,
-			Namespace: "default", // TODO(user):Modify as needed
+	BeforeAll(func() {
+		By("creating the test reconciler")
+		testScheme := scheme.Scheme
+		testScheme.AddKnownTypes(s3oditservicesv1alpha1.GroupVersion, &s3oditservicesv1alpha1.S3Server{})
+		testScheme.AddKnownTypes(s3oditservicesv1alpha1.GroupVersion, &s3oditservicesv1alpha1.S3Bucket{})
+		testReconciler = &S3BucketReconciler{
+			Client: k8sClient,
+			Scheme: testScheme,
+			logger: zap.NewNop().Sugar(),
+			S3ClientFactory: &mocks.S3ClientFactoryMocked{
+				S3ClientMockEnv: &s3MockEnv,
+				S3ClientMockSpy: s3MockSpy,
+			},
 		}
-		s3bucket := &s3oditservicesv1alpha1.S3Bucket{}
+		By("creating a test s3 server")
+		s3Server = &s3oditservicesv1alpha1.S3Server{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "test-s3-server",
+				Namespace: "default",
+			},
+			Spec: s3oditservicesv1alpha1.S3ServerSpec{
+				Type:     "minio",
+				Endpoint: s3MockEnv.ValidEndpoints[0],
+				TLS:      true,
+				Auth:     s3MockEnv.ValidCredentials[0],
+			},
+		}
+		Expect(k8sClient.Create(ctx, s3Server)).To(Succeed())
+	})
 
-		BeforeEach(func() {
-			By("creating the custom resource for the Kind S3Bucket")
-			err := k8sClient.Get(ctx, typeNamespacedName, s3bucket)
-			if err != nil && errors.IsNotFound(err) {
-				resource := &s3oditservicesv1alpha1.S3Bucket{
-					ObjectMeta: metav1.ObjectMeta{
-						Name:      resourceName,
+	Describe("Testing the reconcoile function", func() {
+		Describe("Testing the reconciliation of a new s3bucket", func() {
+			When("A new valid s3bucket is created with a valid s3server", func() {
+				var err error
+				var result ctrl.Result
+				var s3Bucket s3oditservicesv1alpha1.S3Bucket
+
+				BeforeAll(func() {
+					s3MockSpy = &mocks.S3ClientMockSpy{}
+					nameSpacedName := types.NamespacedName{
+						Name:      "test-s3-bucket-nonexistent",
 						Namespace: "default",
-					},
-					// TODO(user): Specify other spec details if needed.
-				}
-				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
-			}
-		})
+					}
+					s3Bucket = s3oditservicesv1alpha1.S3Bucket{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      nameSpacedName.Name,
+							Namespace: nameSpacedName.Namespace,
+						},
+						Spec: s3oditservicesv1alpha1.S3BucketSpec{
+							ServerRef: s3oditservicesv1alpha1.ServerReference{
+								Name:      s3Server.Name,
+								Namespace: s3Server.Namespace,
+							},
+							Region:        "eu-west-1",
+							ObjectLocking: false,
+						},
+					}
+					Expect(k8sClient.Create(ctx, &s3Bucket)).To(Succeed())
 
-		AfterEach(func() {
-			// TODO(user): Cleanup logic after each test, like removing the resource instance.
-			resource := &s3oditservicesv1alpha1.S3Bucket{}
-			err := k8sClient.Get(ctx, typeNamespacedName, resource)
-			Expect(err).NotTo(HaveOccurred())
+					result, err = testReconciler.Reconcile(ctx, ctrl.Request{
+						NamespacedName: nameSpacedName,
+					})
+					Expect(k8sClient.Get(ctx, nameSpacedName, &s3Bucket)).To(Succeed())
+				})
 
-			By("Cleanup the specific resource instance S3Bucket")
-			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
-		})
-		It("should successfully reconcile the resource", func() {
-			By("Reconciling the created resource")
-			controllerReconciler := &S3BucketReconciler{
-				Client: k8sClient,
-				Scheme: k8sClient.Scheme(),
-			}
-
-			_, err := controllerReconciler.Reconcile(ctx, reconcile.Request{
-				NamespacedName: typeNamespacedName,
+				It("Should not return an error", func() {
+					Expect(err).ToNot(HaveOccurred())
+				})
+				It("should return a result with requeue set higher than 0", func() {
+					Expect(result.RequeueAfter).To(BeNumerically(">", 0))
+				})
+				It("should set the status condition to type ready", func() {
+					Expect(s3Server.Status.Conditions[len(s3Server.Status.Conditions)-1].Type).To(Equal(s3oditservicesv1alpha1.ConditionReady))
+				})
+				It("Should call the bucket exists function once", func() {
+					Expect(s3MockSpy.BucketExistsCalled).To(Equal(1))
+				})
+				It("Should call the make bucket function once", func() {
+					Expect(s3MockSpy.MakeBucketCalled).To(Equal(1))
+				})
 			})
-			Expect(err).NotTo(HaveOccurred())
-			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
-			// Example: If you expect a certain status condition after reconciliation, verify it here.
 		})
 	})
 })
