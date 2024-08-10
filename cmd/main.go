a9ac361631736942125f40bbe57700e947b11035
Nicolai Ort <info@nicolai-ort.com>
2024-08-10 19:35:02 +0200

    feat(api): Added new cr bucket

--- cmd/main.go
+++ cmd/main.go
@@ -129,6 +129,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "S3Server")
 		os.Exit(1)
 	}
+	if err = (&controller.S3BucketReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "S3Bucket")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
