db6c526ab002cd8e9a3313f2f3e9eda412b8ba5e
Nicolai Ort <info@nicolai-ort.com>
2026-05-01 14:16:56 +0200

    feat(api): add webhook marker for S3Server
    Refs: #5

--- api/v1alpha1/s3server_types.go
+++ api/v1alpha1/s3server_types.go
@@ -87,6 +87,8 @@ type S3ServerAuthSpec struct {
 // +kubebuilder:printcolumn:name="State",type="string",JSONPath=".status.state",description="The current state of the resource"
 // +kubebuilder:printcolumn:name="LastAction",type="string",JSONPath=".status.lastAction",description="The last action taken on the resource"
 
+// +kubebuilder:webhook:path=/validate-s3-odit-services-v1alpha1-s3server,mutating=false,failurePolicy=fail,sideEffects=None,groups=s3.odit.services,resources=s3servers,verbs=create;update,versions=v1alpha1,name=vs3server.s3.odit.services,admissionReviewVersions=v1
+
 // S3Server is the Schema for the s3servers API
 type S3Server struct {
 	metav1.TypeMeta   `json:",inline" yaml:",inline"`
